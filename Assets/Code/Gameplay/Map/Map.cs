0ae336074f456930dfa52a653bb90f9a813d88ce
Leandro Sebastian Rodriguez <leandro.s.rodriguez@outlook.com>
2025-05-06 17:37:31 -0300

    Map gameplay fundation

---
+++ Assets/Code/Gameplay/Map/Map.cs
@@ -0,0 +1,19 @@
+﻿using AnticTest.Architecture.GameLogic;
+using AnticTest.Architecture.Map;
+using AnticTest.Architecture.Services;
+using UnityEngine;
+
+namespace AnticTest.Gameplay.Map
+{
+	[RequireComponent(typeof(Grid))]
+	public class Map : MonoBehaviour
+	{
+		private Grid<Cell> LogicalGrid => ServiceProvider.Instance.GetService(typeof(Grid<Cell>)) as Grid<Cell>;
+		private Grid gameGrid;
+
+		private void Start()
+		{
+			gameGrid = GetComponent<Grid>();
+		}
+	}
+}
