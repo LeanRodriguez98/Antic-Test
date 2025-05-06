c8934173a22e5b6c82376c07435f689124394d6f
Leandro Sebastian Rodriguez <leandro.s.rodriguez@outlook.com>
2025-05-06 19:01:38 -0300

    Grid Utils fundation

--- Assets/Code/Gameplay/Map/GameMap.cs
+++ Assets/Code/Gameplay/Map/GameMap.cs
@@ -6,14 +6,21 @@ using UnityEngine;
 namespace AnticTest.Gameplay.Map
 {
 	[RequireComponent(typeof(Grid))]
-	public class Map : MonoBehaviour
+	public class GameMap : MonoBehaviour, IService
 	{
 		private Grid<Cell> LogicalGrid => ServiceProvider.Instance.GetService(typeof(Grid<Cell>)) as Grid<Cell>;
 		private Grid gameGrid;
+		[SerializeField] private float cellheight;
 
 		private void Start()
 		{
 			gameGrid = GetComponent<Grid>();
+			ServiceProvider.Instance.AddService(typeof(GameMap), this);
+		}
+
+		public float GetMapHeight() 
+		{
+			return cellheight;
 		}
 	}
 }
---
+++ Assets/Code/Gameplay/Utils/GridUtils.cs
@@ -0,0 +1,42 @@
+﻿using AnticTest.Architecture.Map;
+using AnticTest.Architecture.Services;
+using AnticTest.Gameplay.Map;
+using UnityEngine;
+
+namespace AnticTest.Gameplay.Utils
+{
+	public class GridUtils
+	{
+		public static Coordinate ToCoordinate(Vector2Int vector2Int)
+		{
+			return new Coordinate(vector2Int.x, vector2Int.y);
+		}
+
+		public static Coordinate ToCoordinate(Vector3Int vector3Int)
+		{
+			return new Coordinate(vector3Int.x, vector3Int.y);
+		}
+
+		public static Vector2Int ToVector2Int(Coordinate coordinate)
+		{
+			return new Vector2Int(coordinate.x, coordinate.y);
+		}
+
+		public static Vector3Int ToVector3Int(Coordinate coordinate)
+		{
+			return new Vector3Int(coordinate.x, coordinate.y);
+		}
+
+		public static Vector3Int ToVector3Int(Vector2Int vector2Int)
+		{
+			return new Vector3Int(vector2Int.x, vector2Int.y);
+		}
+
+		public static Vector3 CoordinateToWorld(Coordinate coordinate)
+		{
+			GameMap gameMap = ServiceProvider.Instance.GetService(typeof(GameMap)) as GameMap;
+			Grid grid = ServiceProvider.Instance.GetService(typeof(Grid)) as Grid;
+			return grid.CellToWorld(ToVector3Int(coordinate)) + Vector3.up * gameMap.GetMapHeight();
+		}
+	}
+}
